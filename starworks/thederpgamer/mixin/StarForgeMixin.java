e8a1ac3a6a63b1871ea5af238852515d150c9298
TheDerpGamer <garret2kool@gmail.com>
2019-03-25 12:31:52 -0700

    Create StarForgeMixin.java

---
+++ starworks/thederpgamer/mixin/StarForgeMixin.java
@@ -0,0 +1,21 @@
+package starforge.thederpgamer.mixin;
+
+import org.schema.game.client.controller.GameClientController;
+import org.schema.schine.network.client.ClientController;
+import org.schema.schine.network.client.ClientState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameClientController.class)
+public abstract class StarForgeMixin extends ClientController {
+	private ExampleMixin(ClientState clientState) {
+		super(clientState);
+	}
+
+	@Inject(at = @At("HEAD"), method = "initialize()V")
+	public void init(CallbackInfo info) {
+		System.out.println("StarForge API Mixin Loaded!");
+	}
+}
