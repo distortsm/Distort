95de765e3699099315d29cc7ebe7d34e776bb532
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 09:35:06 -0700

    Update Main.java

--- net/distortsm/api/main/Main.java
+++ net/distortsm/api/main/Main.java
@@ -3,6 +3,9 @@ package net.distortsm.api.main;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
+
+public static final String starmadeDirectory = "../";
+
 @Override
   public void onInitialize() {
     System.out.println("Distort API is ready!");
