c762d85b5ed9217960c7e8dfc758927cab9b93da
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 09:17:06 -0700

    Update Player.java

--- net/distortsm/api/player/Player.java
+++ net/distortsm/api/player/Player.java
@@ -3,13 +3,14 @@ package net.distortsm.api.player;
 import net.distortsm.api.server.Server;
 import net.distortsm.api.guis.ErrorBox;
 import net.distortsm.api.guis.MessageBox;
+import net.distortsm.api.entity.Entity;
 
 public class Player {
 
   public static Server server = new Server();
   public static Player player = new Player();
 
-  public static void sendErrorBox(ErrorBox errorBox) {
+  private static void sendErrorBox(ErrorBox errorBox) {
     if(server.getPlayer(player) == null) {
       System.out.println(player + " isn't on server! Cannot send ErrorBox");
     } else {
@@ -18,12 +19,16 @@ public class Player {
     }
   }
 
-  public static void sendMessageBox(MessageBox messageBox) {
+  private static void sendMessageBox(MessageBox messageBox) {
     if(server.getPlayer(player) == null) {
       System.out.println(player + " isn't on server! Cannot send MessageBox");
     } else {
       //figure out how to send messageBox to a player
       System.out.println("Sent MessageBox to " + player);
     }
+  }
+
+  public static getCurrentEntity(Player player) {
+    /** Gets the current entity a player is aligned/attached to */
   }
 }
