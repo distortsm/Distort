83efce52b86b649e9b3aa6344615c808e908be47
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 12:26:04 -0700

    Update Ship.java

--- net/distortsm/api/entity/Ship.java
+++ net/distortsm/api/entity/Ship.java
@@ -2,17 +2,44 @@ package net.distortsm.api.entity;
 
 import net.distortsm.api.entity.Entity;
 import net.distortsm.api.systems.Reactor;
+import net.distortsm.api.systems.ShieldGroup;
 
 public class Ship {
 
   public static Entity ship = new Entity();
   public static Reactor reactor = new Reactor();
+  public static ShieldGroup shieldGroup = new ShieldGroup();
+  private static double reactorRegen;
+  private static double reactorStabilization;
+  private static double reactorUsage;
+  private static double shieldRegen;
+  private static double shieldCapacity;
 
   public static getReactor(Reactor reactor) {
 
   }
 
   public static getCurrentReactor(Ship ship) {
+
+  }
+
+  public static getReactorRegen(Reactor reactor) {
+
+  }
+
+  public static getReactorStabilization(Reactor reactor) {
+
+  }
+
+  public static getReactorUsage(Reactor reactor) {
+
+  }
+
+  public static getShieldCapcity(ShieldGroup shieldGroup) {
+
+  }
+
+  public static getShieldRegen(ShieldGroup shieldGroup) {
 
   }
 }
