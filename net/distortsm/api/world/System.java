b27abb9baaa8819eb5f1a810ca8f3b83c8efab61
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 09:45:16 -0700

    Update System.java

--- net/distortsm/api/world/System.java
+++ net/distortsm/api/world/System.java
@@ -1,4 +1,9 @@
 package net.distortsm.api.world;
 
 public class System {
+
+  public static System system = new System();
+  public static ArrayList<Integer> systemCoords = new ArrayList<Integer>();
+
+  
 }
