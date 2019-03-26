898bea0e8d572b3260a4bd458dcc02fd1859e476
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 12:10:59 -0700

    Update Station.java

--- net/distortsm/api/entity/Station.java
+++ net/distortsm/api/entity/Station.java
@@ -1,9 +1,19 @@
 package net.distortsm.api.entity;
 
 import net.distortsm.api.entity.Entity;
+import net.distortsm.api.systems.Reactor;
 
 public class Station {
 
   public static Entity station = new Entity();
+  public static Reactor reactor = new Reactor();
+
+  public static getReactor(Reactor reactor) {
+
+  }
+
+  public static getCurrentReactor(Station station) {
+
+  }
 
 }
