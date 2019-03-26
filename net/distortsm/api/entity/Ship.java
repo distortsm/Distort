63c329a701e71bb503ad3615c05496d891680459
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 12:10:57 -0700

    Update Ship.java

--- net/distortsm/api/entity/Ship.java
+++ net/distortsm/api/entity/Ship.java
@@ -1,9 +1,18 @@
 package net.distortsm.api.entity;
 
 import net.distortsm.api.entity.Entity;
+import net.distortsm.api.systems.Reactor;
 
 public class Ship {
 
   public static Entity ship = new Entity();
+  public static Reactor reactor = new Reactor();
 
+  public static getReactor(Reactor reactor) {
+
+  }
+
+  public static getCurrentReactor(Ship ship) {
+
+  }
 }
