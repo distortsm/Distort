a29e7b6591b7880cc71b91e6232d0ce24e4695fb
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 12:35:16 -0700

    Update Reactor.java

--- net/distortsm/api/systems/Reactor.java
+++ net/distortsm/api/systems/Reactor.java
@@ -5,10 +5,10 @@ public class Reactor {
   public static Reactor reactor = new Reactor();
 
   public static getRegen(Reactor reactor) {
-    /** Gets the regen of the specified reactor */
+
   }
 
   public static getStabilization(Reactor reactor) {
-    /** Gets the stabilization of the specified reactor */
+    
   }
 }
