c8825494e661a5e354bf76cd540a74356befb5d1
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 15:16:23 -0700

    Create AccessModifier.java

---
+++ net/distortsm/api/main/AccessModifier.java
@@ -0,0 +1,13 @@
+package net.distortsm.api.main;
+
+public class AccessModifier {
+
+  public static void main() {
+    Field playerStateCurrentSector = PlayerState.class.getDeclaredField(currentSector);
+    playerStateCurrentSector.setAccessible(true);
+    Field playerStateCurrentSystem = PlayerState.class.getDeclaredField(currentSystem);
+    playerStateCurrentSystem.setAccessible(true);
+    Field playerStateName = PlayerState.class.getDeclaredField(name);
+    playerStateName.setAccessible(true);
+  }
+}
