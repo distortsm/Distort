00263e05fb1ad6508140fc9f1854c2d3bd85fbde
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 07:58:35 -0700

    Create Server.java

---
+++ net/distortsm/api/server/Server.java
@@ -0,0 +1,12 @@
+package net.distortsm.api.server;
+
+import net.distortsm.api.Player;
+
+public static class Server {
+
+  public static void getPlayer(Player p) {
+    //figure out how to get the player specified
+
+    return(p);
+  }
+}
