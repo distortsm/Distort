1d532cd54ae0efe9a9236993a44a88e40c1b5341
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 12:35:13 -0700

    Update Station.java

--- net/distortsm/api/entity/Station.java
+++ net/distortsm/api/entity/Station.java
@@ -8,12 +8,30 @@ public class Station {
 
   public static Entity station = new Entity();
   public static Reactor reactor = new Reactor();
+  public static ShieldGroup shieldGroup = new ShieldGroup();
+  private static double reactorRegen;
+  private static double reactorStabilization;
+  private static double reactorUsage;
+  private static double shieldRegen;
+  private static double shieldCapacity;
 
   public static getReactor(Reactor reactor) {
 
   }
 
-  public static getCurrentReactor(Station station) {
+  public static getCurrentReactor(Ship ship) {
+
+  }
+
+  public static getReactorRegen(Reactor reactor) {
+
+  }
+
+  public static getReactorStabilization(Reactor reactor) {
+
+  }
+
+  public static getReactorUsage(Reactor reactor) {
 
   }
 
