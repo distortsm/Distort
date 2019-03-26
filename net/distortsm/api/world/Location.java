7668446482ccc00285f601d0cc8c624261638fe3
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 09:17:08 -0700

    Create Location.java

---
+++ net/distortsm/api/world/Location.java
@@ -0,0 +1,10 @@
+package net.distortsm.api.world;
+
+public class Location {
+
+  public static Location location = new Location();
+
+  public static getBlock(Location location) {
+    /** Gets the block at a specified location */
+  }
+}
