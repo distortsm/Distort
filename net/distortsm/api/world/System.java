fbbd533851397a58faf3e6a35a43e89257ed3383
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 10:32:42 -0700

    Update System.java

--- net/distortsm/api/world/System.java
+++ net/distortsm/api/world/System.java
@@ -4,6 +4,7 @@ public class System {
 
   public static System system = new System();
   public static ArrayList<Integer> systemCoords = new ArrayList<Integer>();
-
-  
+  public static int systemx = systemCoords[0];
+  public static int systemy = systemCoords[1];
+  public static int systemz = systemCoords[2];
 }
