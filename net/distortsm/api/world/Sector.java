7623ce33d9049fec9145a25a3ce86afc89f08722
TheDerpGamer <garret2kool@gmail.com>
2019-03-26 14:56:37 -0700

    Delete Sector.java

--- net/distortsm/api/world/Sector.java
+++
@@ -1,8 +0,0 @@
-package net.distortsm.api.world;
-
-public class Sector {
-
-  public static Sector sector = new Sector();
-  public static ArrayList<Integer> sectorCoords = new ArrayList<Integer>();
-  
-}
