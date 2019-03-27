b437ae956721572efd6595a45521e213cd3ee8bf
TheDerpGamer <garret2kool@gmail.com>
2019-03-27 09:21:59 -0700

    updated systems.java updated entity.java

--- net/distortsm/api/entity/Ship.java
+++ net/distortsm/api/entity/Ship.java
@@ -15,31 +15,55 @@ public class Ship {
   private static double shieldRegen;
   private static double shieldCapacity;
 
-  public static getReactor(Reactor reactor) {
-
+  public static getReactorRegen() {
+	  return Reactor.reactorReacharge;
   }
-
-  public static getCurrentReactor(Ship ship) {
-
+  
+  public static getReactorRegenPercent() {
+	  return Reactor.reactorRechargePercent;
   }
-
-  public static getReactorRegen(Reactor reactor) {
-
+  
+  public static getReactorCurrentConsumption() {
+	  return Reactor.reactorCurentConsumption;
   }
-
-  public static getReactorStabilization(Reactor reactor) {
-
+  
+  public static getReactorConsumptionPercent() {
+	  return Reactor.powerConsumptionAsPercent;
   }
-
-  public static getReactorUsage(Reactor reactor) {
-
+  
+  public static getReactorSwitchCooldown() {
+	  return Reactor.reactorSwitchCooldown;
   }
-
-  public static getShieldCapcity(ShieldGroup shieldGroup) {
-
+  
+  public static getReactorRebootCooldown() {
+	  return Reactor.reactorRebootCooldown;
   }
-
-  public static getShieldRegen(ShieldGroup shieldGroup) {
-
+  
+  public static getReactorRebootTimeSec() {
+	  return Reactor.rebootTimeSec;
+  }
+  
+  public static getActiveReactorIntegrity() {
+	  return Reactor.activeReactorIntegrity;
+  }
+  
+  public static isDamaged() {
+	  return Reactor.isDamaged;
+  }
+  
+  public static getConsumerList() {
+	  return Reactor.powerConsumerList;
+  }
+  
+  public static getReactorMaxHP() {
+	  return Reactor.reactorMaxHP;
+  }
+  
+  public static getReactorCurrentHP() {
+	  return Reactor.reactorCurrentHP;
+  }
+  
+  public static getReactorChamberCapacity() {
+	  return Reactor.chamberCapacity;
   }
 }
--- net/distortsm/api/systems/Reactor.java
+++ net/distortsm/api/systems/Reactor.java
@@ -1,14 +1,30 @@
 package net.distortsm.api.systems;
 
-public class Reactor {
+import org.schema.game.common.controller.elements.power.reactor.PowerInterface;
+
+public class Reactor implements PowerInterface {
 
   public static Reactor reactor = new Reactor();
-
-  public static getRegen(Reactor reactor) {
-
+  public static double reactorRecharge = getRechargeRatePowerPerSec();
+  public static double reactorRechargePercent = getRechargeRatePercentPerSec();
+  public static double reactorCurentConsumption = getCurrentConsumptionPerSec();
+  public static float reactorSwitchCooldown = getReactorSwitchCooldown();
+  public static float reactorRebootCooldown = getReactorRebootCooldown();
+  public static float rebootTimeSec = getRebootTimeSec();
+  public static double activeReactorIntegrity = getActiveReactorIntegrity();
+  public static boolean isDamaged = isAnyDamaged();
+  public static List powerConsumerList = getPowerConsumerList();
+  public static long reactorMaxHP = getCurrentMaxHp();
+  public static long reactorCurrentHP = getCurrentHp();
+  public static double powerConsumptionAsPercent = getPowerConsumptionAsPercent();
+  public static float chamberCapacity = getChamberCapacity();
+  public static double stabilizationEfficiency = getStabilizerEfficiencyTotal();
+  
+  public static int getMinSizeNeededForReactorLevel(int level) {
+	  getNeededMinForReactorLevel(int level);
   }
-
-  public static getStabilization(Reactor reactor) {
-    
+  
+  public static int getMaxSizeNeededForReactorLevel(int level) {
+	  getNeededMaxForReactorLevel(int level);
   }
 }
