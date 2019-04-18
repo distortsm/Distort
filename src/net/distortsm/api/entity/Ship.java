7d83ae8c9631549c437d17f9e1ba271efe0c1ad4
TheDerpGamer <garret2kool@gmail.com>
2019-04-18 09:43:54 -0700

    Update Ship.java

--- src/net/distortsm/api/entity/Ship.java
+++ src/net/distortsm/api/entity/Ship.java
@@ -9,61 +9,10 @@ public class Ship {
   public static Entity ship = new Entity();
   public static Reactor reactor = new Reactor();
   public static ShieldGroup shieldGroup = new ShieldGroup();
-  private static double reactorRegen;
+  private static double reactorRegen = reactor.getRechargeRatePowerPerSec();
   private static double reactorStabilization;
   private static double reactorUsage;
   private static double shieldRegen;
   private static double shieldCapacity;
 
-  public static getReactorRegen() {
-	  return Reactor.reactorReacharge;
-  }
-  
-  public static getReactorRegenPercent() {
-	  return Reactor.reactorRechargePercent;
-  }
-  
-  public static getReactorCurrentConsumption() {
-	  return Reactor.reactorCurentConsumption;
-  }
-  
-  public static getReactorConsumptionPercent() {
-	  return Reactor.powerConsumptionAsPercent;
-  }
-  
-  public static getReactorSwitchCooldown() {
-	  return Reactor.reactorSwitchCooldown;
-  }
-  
-  public static getReactorRebootCooldown() {
-	  return Reactor.reactorRebootCooldown;
-  }
-  
-  public static getReactorRebootTimeSec() {
-	  return Reactor.rebootTimeSec;
-  }
-  
-  public static getActiveReactorIntegrity() {
-	  return Reactor.activeReactorIntegrity;
-  }
-  
-  public static isDamaged() {
-	  return Reactor.isDamaged;
-  }
-  
-  public static getConsumerList() {
-	  return Reactor.powerConsumerList;
-  }
-  
-  public static getReactorMaxHP() {
-	  return Reactor.reactorMaxHP;
-  }
-  
-  public static getReactorCurrentHP() {
-	  return Reactor.reactorCurrentHP;
-  }
-  
-  public static getReactorChamberCapacity() {
-	  return Reactor.chamberCapacity;
-  }
 }
