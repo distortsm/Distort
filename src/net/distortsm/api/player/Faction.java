48242db18683e18d42417c117251865aa357abd8
TheDerpGamer <garret2kool@gmail.com>
2019-05-11 11:52:52 -0700

    Update Faction.java

--- src/net/distortsm/api/player/Faction.java
+++ src/net/distortsm/api/player/Faction.java
@@ -6,19 +6,4 @@ import org.schema.game.common.data.player.PlayerState;
 
 public class Faction {
 	
-	public static Object getFactionName() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method method = PlayerState.class.getClass().getDeclaredMethod("getFactionName");
-		method.setAccessible(true);
-		Object factionNameObj = method.invoke(getFactionName());
-		String factionName = factionNameObj.toString();
-		return factionName;
-	}
-	
-	public static Object getFactionID() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method method = PlayerState.class.getClass().getDeclaredMethod("getFactionId");
-		method.setAccessible(true);
-		Object factionIDObj = method.invoke(getFactionID());
-		String factionID = factionIDObj.toString();
-		return factionID;
-	}
 }
