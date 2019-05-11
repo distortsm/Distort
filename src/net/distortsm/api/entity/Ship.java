7884cdcde4cff856dbe171b3f8536c79a660e2b1
TheDerpGamer <garret2kool@gmail.com>
2019-05-11 11:53:04 -0700

    Update Ship.java

--- src/net/distortsm/api/entity/Ship.java
+++ src/net/distortsm/api/entity/Ship.java
@@ -1,18 +1,19 @@
 package net.distortsm.api.entity;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.schema.game.common.controller.elements.power.reactor.MainReactorUnit;
+import org.schema.game.common.controller.elements.power.reactor.PowerInterface;
 import net.distortsm.api.entity.Entity;
 import net.distortsm.api.systems.Reactor;
 import net.distortsm.api.systems.ShieldGroup;
 
 public class Ship {
 
-  public static Entity ship = new Entity();
-  public static Reactor reactor = new Reactor();
-  public static ShieldGroup shieldGroup = new ShieldGroup();
-  private static double reactorRegen = reactor.getRechargeRatePowerPerSec();
-  private static double reactorStabilization;
-  private static double reactorUsage;
-  private static double shieldRegen;
-  private static double shieldCapacity;
+	private static Object getReactorObject() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		Method method = PowerInterface.class.getDeclaredMethod("getReactor", MainReactorUnit.class);
+		method.setAccessible(true); 
+		return method.invoke(getReactorObject()); 
+	}
 
 }
