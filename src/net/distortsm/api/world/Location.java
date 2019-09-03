3985495a358f9ce43a6ecfd4209dc98b2704673c
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 00:14:04 -0700

    Delete Location.java

--- src/net/distortsm/api/world/Location.java
+++
@@ -1,9 +0,0 @@
-package net.distortsm.api.world;
-
-import java.util.ArrayList;
-
-public class Location {
-
-  public static Location location = new Location();
-  public static ArrayList<Location> locationCoords = new ArrayList<Location>();
-}
