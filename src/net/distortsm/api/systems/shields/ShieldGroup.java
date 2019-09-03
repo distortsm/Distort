59fda47fffc04c8a62d83e312b5f2f846086c447
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 11:15:02 -0700

    Update ShieldGroup.java

--- src/net/distortsm/api/systems/shields/ShieldGroup.java
+++ src/net/distortsm/api/systems/shields/ShieldGroup.java
@@ -2,4 +2,32 @@ package net.distortsm.api.systems.shields;
 
 public class ShieldGroup {
 
+	private static double shieldCapacity;
+	private static double shieldRegen;
+	private static double shieldRadius;
+	
+	public static double getCapacity() {
+		return shieldCapacity;
+	}
+	
+	public static double getRegen() {
+		return shieldRegen;
+	}
+	
+	public static double getRadius() {
+		return shieldRadius;
+	}
+	
+	public static void setCapacity(double capacity) {
+		shieldCapacity = capacity;
+	}
+	
+	public static void setRegen(double regen) {
+		shieldRegen = regen;
+	}
+	
+	public static void setRadius(double radius) {
+		shieldRadius = radius;
+	}
+
 }
