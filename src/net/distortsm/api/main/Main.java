fc23affa73b1d23ed9363bf63ea1342ef257069b
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 00:13:45 -0700

    Update Main.java

--- src/net/distortsm/api/main/Main.java
+++ src/net/distortsm/api/main/Main.java
@@ -6,9 +6,9 @@ import java.util.UUID;
 
 public class Main implements ModInitializer {
 
-public static final String starmadeDirectory = "../";
-public static final String modsFolder = starmadeDirectory + "mods";
-public static ArrayList<Mod> mods = new ArrayList<Mod>();
+public final String starmadeDirectory = "../";
+public final String modsFolder = starmadeDirectory + "mods";
+public ArrayList<Mod> mods = new ArrayList<Mod>();
 
 @Override
   public void onInitialize() {
