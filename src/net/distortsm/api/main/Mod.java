66142f7d4852114826fb0729d243e2fefcefddbe
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 00:13:49 -0700

    Update Mod.java

--- src/net/distortsm/api/main/Mod.java
+++ src/net/distortsm/api/main/Mod.java
@@ -1,8 +1,13 @@
 package net.distortsm.api.main;
 
+import java.util.ArrayList;
+
 public class Mod {
 
 	public static String modName;
 	public static String modAuthor;
+	public static String modDescription;
 	public static String modVersion;
+	public static String modSMVersion;
+	public static ArrayList<Mod> modDependencies;
 }
