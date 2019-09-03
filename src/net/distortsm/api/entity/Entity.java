65ea867b707b3acd0b510c3c8bdcda0467fc4ee6
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 00:13:04 -0700

    Update Entity.java

--- src/net/distortsm/api/entity/Entity.java
+++ src/net/distortsm/api/entity/Entity.java
@@ -1,20 +1,8 @@
 package net.distortsm.api.entity;
 
-import net.distortsm.api.block.Block;
-import net.distortsm.api.world.Location;
-
 public class Entity {
 
-  public static Entity entity = new Entity();
-  
-  public static Block getBlockAt(Location[] location) {
-    Block block;
-    int blockx = location.locationCoords[0];
-    int blocky = location.locationCoords[1];
-    int blockz = location.locationCoords[2];
-
-    //get the block at blockx, blocky, blockz
-
-    return block;
-  }
+	private String entityName;
+	private EntityType entityType;
+	private String entityID;
 }
