27d676e258cb3e1196779c6aa8ab8bb0dff8c125
TheDerpGamer <garret2kool@gmail.com>
2019-09-03 00:13:58 -0700

    Create Universe.java

---
+++ src/net/distortsm/api/universe/Universe.java
@@ -0,0 +1,13 @@
+package net.distortsm.api.universe;
+
+import net.distortsm.api.entity.Entity;
+
+public class Universe {
+	
+	public static Entity getEntityFromID(String ID) {
+		
+		
+		return entity;
+	}
+	
+}
