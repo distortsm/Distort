9215c209ff57f9291daf1a05f41d3f1220c32ba3
TheDerpGamer <garret2kool@gmail.com>
2019-09-23 16:03:22 -0700

    Create Registry.java

---
+++ src/net/distortsm/api/mixin/Registry.java
@@ -0,0 +1,24 @@
+package net.distortsm.api.mixin;
+
+import net.distortsm.api.element.Block;
+import net.distortsm.api.element.Element;
+import net.distortsm.api.element.Item;
+
+public class Registry {
+	
+	public static void addBlock(Block block) {
+		
+	}
+	
+	public static void addItem(Item item) {
+		
+	}
+	
+	public static void addWeapon(Weapon weapon) {
+		
+	}
+	
+	public static void addElement(Element element) {
+		
+	}
+}
