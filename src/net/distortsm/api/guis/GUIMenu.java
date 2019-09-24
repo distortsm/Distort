c119639de958d4d7f5e183a01ee220bc5109a8ad
TheDerpGamer <garret2kool@gmail.com>
2019-09-24 10:09:05 -0700

    Create GUIMenu.java

---
+++ src/net/distortsm/api/guis/GUIMenu.java
@@ -0,0 +1,50 @@
+package net.distortsm.api.guis;
+
+import java.util.Observable;
+import java.util.Observer;
+import org.schema.schine.graphicsengine.forms.gui.GUIElement;
+import org.schema.schine.input.InputState;
+
+public class GUIMenu extends GUIElement implements Observer{
+
+	public GUIMenu(InputState var1) {
+		super(var1);
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
