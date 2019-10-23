852925252d33b7a7ab757efbcd1bdc8cdf45a28e
TheDerpGamer <garret2kool@gmail.com>
2019-10-22 19:07:24 -0700

    Update Ship.java

--- src/net/distortsm/api/entity/Ship.java
+++ src/net/distortsm/api/entity/Ship.java
@@ -9,21 +9,27 @@ import net.distortsm.api.systems.shields.ShieldGroup;
 
 public class Ship extends Entity {
 
-	private EntityType entityType = EntityType.SHIP;
 	private double thrustMassRatio;
 	private double thrusterCount;
 	private double turningSpeed;
-	private Faction faction;
+	private Faction entityFaction;
 	private List<ShieldGroup> shieldgroups;
 	private List<Ship> turrets;
 	private List<Ship> docks;
 	private List<Reactor> reactors;
 	private Reactor currentReactor;
-	
+
+	public Ship(String name, String ID, Faction faction) {
+		entityName = name;
+		entityType = EntityType.SHIP;
+		entityID = ID;
+		entityFaction = faction;
+	}
+
 	//Object Reflectors
 	private Object getCurrentReactorObject(long var1) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 		Method method = Ship.class.getDeclaredMethod("getReactor", long.class);
-		method.setAccessible(true); 
-		return method.invoke(getReactorObject(var1)); 
+		method.setAccessible(true);
+		return method.invoke(getCurrentReactorObject(var1));
 	}
 }
