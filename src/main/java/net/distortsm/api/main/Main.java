991ace28905c8af8c918c0f218ea512b36714c2d
TheDerpGamer <garret2kool@gmail.com>
2020-01-25 12:39:24 -0700

    Update Main.java

--- src/main/java/net/distortsm/api/main/Main.java
+++ src/main/java/net/distortsm/api/main/Main.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 public class Main implements ModInitializer {
 
-public final String starmadeDirectory = "../";
-public final String modsFolder = starmadeDirectory + "mods";
-public ArrayList<Mod> mods = new ArrayList<Mod>();
+  public final String starmadeDirectory = "../";
+  public final String modsFolder = starmadeDirectory + "mods";
+  public ArrayList<Mod> mods = new ArrayList<Mod>();
 
-@Override
+  @Override
   public void onInitialize() {
     System.out.println("Distort API is ready!");
   }
