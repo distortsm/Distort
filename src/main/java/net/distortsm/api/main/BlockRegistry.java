f3b40d1badede09020cc772760bafa3f63395c3c
TheDerpGamer <garret2kool@gmail.com>
2020-01-25 12:21:19 -0700

    cleaning up gradle part 1

--- src/main/java/net/distortsm/api/main/BlockRegistry.java
+++ src/main/java/net/distortsm/api/main/BlockRegistry.java
@@ -1,9 +1,7 @@
 package net.distortsm.api.main;
 
 import net.distortsm.api.element.Block;
-import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin()
 public class BlockRegistry {
 
     public static void registerBlock(Block block) {
